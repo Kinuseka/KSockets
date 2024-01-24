87f2bb31c2e09b8ff3049df2df9ad5600a54f9e8
Milbert Jr Macarambon <mmacarambon@kinuseka.us>
2024-01-25 03:30:22 +0800

    Thread safe, and organized structure

--- SimpleSocket/model_api.py
+++ SimpleSocket/model_api.py
@@ -1,27 +1,137 @@
+"""
+SimpleSocket.model_api
+~~~~~~~~~~~~~
+Lower level api for socket communication
+"""
+from functools import wraps
+import threading
+import socket
+import json
+import time
 from .exceptions import client_protocol_mismatch
 from .constants import Constants as cnts
 from .packers import formatify, decodify
-import socket
-import json
-import threading
-import time
 
-def synchronized(func):
+rx_lock = threading.Lock()
+tx_lock = threading.Lock()
+def synchronized_tx(func):
+    """
+    Make transmitting thread safe
+    """
     # Decorator to make the function thread-safe
-    lock = threading.Lock()
+    @wraps(func)
     def wrapper(*args, **kwargs):
-        with lock:
-            return func(*args, **kwargs)
+        thread_lock = kwargs.get("thread_lock", None)
+        if isinstance(thread_lock, bool) and thread_lock or thread_lock is None:
+            with tx_lock:
+                return func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper
+def synchronized_rx(func):
+    """
+    Make receiving thread safe
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        thread_lock = kwargs.get("thread_lock", None)
+        if isinstance(thread_lock, bool) and thread_lock or thread_lock is None:
+            with rx_lock:
+                return func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper
 
-class Client_Main:
+class SocketModel:
+    def __init__(self) -> None:
+        self.chunk_size = None
+        self.header_chunksize = None
+        self.socket: socket.socket = None
+    @synchronized_tx
+    def send(self, data, client: socket.socket = None, **kwargs):
+        """
+        Low level function, handling protocol transmission
+        """
+        client_target = client if client else self.socket
+        template = {
+            'a': len(data),
+            'r': self.chunk_size
+        }
+        if len(data) > self.chunk_size:
+            client_target.send(formatify(template, padding=self.header_chunksize))
+            chunks = [data[i:i+self.chunk_size] for i in range(0, len(data), self.chunk_size)]
+            for chunk in chunks:
+                client_target.send(chunk)
+        else:
+            template['r'] = len(data)
+            client_target.send(formatify(template, padding=self.header_chunksize))
+            client_target.send(data)
+    @synchronized_rx
+    def receive(self, client: socket.socket = None, **kwargs):
+        """
+        Low level function, handling protocol incoming data
+        """
+        client_target = client if client else self.socket
+        received_bytes = self._recvall(client_target, self.header_chunksize)
+        if not received_bytes:
+            return False
+        try:
+            header = decodify(received_bytes, padding=self.header_chunksize)
+            total_len = header['a']
+            chunked = header['r']
+        except (KeyError, json.decoder.JSONDecodeError):
+            return b''
+        total_received = 0
+        if not chunked > self.chunk_size:
+            #Reject message if client disrespects negotiated chunk size
+            data = b''
+            data_left = total_len
+            while not total_received >= total_len:
+                if data_left > self.chunk_size:
+                    chunked = self.chunk_size
+                    data_left -= self.chunk_size
+                else:
+                    chunked = data_left
+                new_data = self._recvall(client_target, chunked)
+                if not new_data:
+                    break
+                data += new_data
+                total_received = len(data)
+            return data
+        else: 
+            return b''
+        
+    def _recvall(self, client: socket.socket, byte_target):
+        data = bytearray()
+        while len(data) < byte_target:
+            packet = client.recv(byte_target - len(data))
+            if not packet:
+                return None
+            data.extend(packet)
+        return data
+
+    def unsafe_receive(self, client: socket.socket = None):
+        """
+        Receive data without thread safety.
+        Potentially destructive and might cause errors
+        """
+        return self.receive(client=client, thread_lock=False)
+    
+    def unsafe_send(self, data, client: socket.socket = None):
+        """
+        Send data without thread safety.
+        Potentially destructive and might cause errors
+        """
+        return self.send(data=data, client=client, thread_lock=False)
+
+
+class Client_Main(SocketModel):
     def __init__(self, socket: socket.socket = socket.socket(), address: tuple = None, chunk_size = 1024) -> None:
         '''
         socket: Socket object
         address: Destination address to connect to
         '''
+        super().__init__()
         self.address = address
-        self.data_chunksize = chunk_size # Suggested chunksize but server might enforce a preferred one
+        self.chunk_size = chunk_size # Suggested chunksize but server might enforce a preferred one
         self.header_chunksize = cnts.HEADER_CHUNKS
         #Init Socket
         self.socket = socket
@@ -35,61 +145,17 @@ class Client_Main:
                 _packed_msg = json.dumps({'ch': self.header_chunksize})
                 self.socket.sendall(_packed_msg.encode('utf-8'))
             else:
-                self.data_chunksize = _initial_header
+                self.chunk_size = _initial_header
         except json.decoder.JSONDecodeError as e:
             raise client_protocol_mismatch("Client cannot decode server's initial response. The client might be outdated or the server is invalid", property=self)
         except KeyError as e:
             raise client_protocol_mismatch("Header is decoded but cannot find the proper Key. The module might be outdated, Err:{}".format(e), property=self)
         #Header Chunksize is equal to the length of chunksize
-
-    #Functions
-    @synchronized
-    def send(self, data):
-        """
-        Low level function, handling protocol sends
-        """
-        template = {
-            'a': len(data),
-            'r': self.data_chunksize
-        }
-        if len(data) > self.data_chunksize:
-            self.socket.sendall(formatify(template, padding=self.header_chunksize))
-            chunks = [data[i:i+self.data_chunksize] for i in range(0, len(data), self.data_chunksize)]
-            for chunk in chunks:
-                self.socket.sendall(chunk)
-        else:
-            template['r'] = len(data)
-            self.socket.sendall(formatify(template, padding=self.header_chunksize))
-            self.socket.sendall(data)
-    
-    @synchronized
-    def receive(self) -> bytes:
-        # Receive the header indicating the size of the chunk
-        # Receive the chunk
-        received_bytes = self.socket.recv(self.header_chunksize)
-        if not received_bytes:
-            return b''
-        header = decodify(received_bytes, padding=self.header_chunksize)
-        total_len = header['a']
-        chunked = header['r']
-        total_received = 0
-        if not chunked > self.data_chunksize:
-            #Reject message if server disrespects negotiated chunk size
-            data = b''
-            while total_received != total_len:
-                if total_len > self.data_chunksize:
-                    chunked = total_len - len(data)
-                data += self.socket.recv(chunked)
-                total_received += len(data)
-            return data
-        else:
-            return b''
     
     def close(self):
         self.socket.close()
         
-
-class Server_Main:
+class Server_Main(SocketModel):
     def __init__(self, socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM), address: tuple = ('127.0.0.1', 3010), chunk_size = 1024, enforce_chunks = True) -> None:
         '''
         socket: Socket object
@@ -97,6 +163,7 @@ class Server_Main:
         chunk_size: Size of the message chunks
         enforce_chunks: Force clients to use your chunk_size option [Recommended: True]
         '''
+        super().__init__()
         self.address = address
         self.chunk_size = chunk_size
         self.enforce_chunks = enforce_chunks
@@ -122,53 +189,9 @@ class Server_Main:
                 if request.get("req") == "request-head":
                     client.sendall(formatify({'ch': self.chunk_size}, padding=1024))
                     return (client, address) 
-            except (socket.timeout, BlockingIOError, OSError) as e:
+            except (socket.timeout, BlockingIOError, OSError):
                 time.sleep(0.5)
                 continue      
-    
-    def send(self, data: bytes, client: socket.socket):
-        """
-        Low level function, handling protocol sends
-        """
-        template = {
-            'a': len(data),
-            'r': self.chunk_size
-        }
-        if len(data) > self.chunk_size:
-            client.sendall(formatify(template, padding=self.header_chunksize))
-            chunks = [data[i:i+self.chunk_size] for i in range(0, len(data), self.chunk_size)]
-            for chunk in chunks:
-                client.sendall(chunk)
-        else:
-            template['r'] = len(data)
-            client.sendall(formatify(template, padding=self.header_chunksize))
-            client.sendall(data)
-
-    def receive(self, client: socket.socket):
-        received_bytes = client.recv(self.header_chunksize)
-        if not received_bytes:
-            return b''
-        try:
-            header = decodify(received_bytes, padding=self.header_chunksize)
-            total_len = header['a']
-            chunked = header['r']
-        except (KeyError, json.decoder.JSONDecodeError):
-            return b''
-        total_received = 0
-        if not chunked > self.chunk_size:
-            #Reject message if client disrespects negotiated chunk size
-            data = b''
-            while not total_received >= total_len:
-                if total_len > self.chunk_size:
-                    chunked = total_len - len(data)
-                new_data = client.recv(chunked)
-                if not new_data:
-                    break
-                data += new_data
-                total_received = len(data)
-            return data
-        else: 
-            return b''
-    
+        
     def close(self):
         self.socket.close()
