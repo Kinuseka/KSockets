df4a231bec8b590606ecf18128faa653f1ab6a1a
Milbert Jr Macarambon <mmacarambon@kinuseka.us>
2024-08-03 15:37:12 +0800

    Rename to KSockets

---
+++ KSockets/exceptions.py
@@ -0,0 +1,28 @@
+class SocketException(BaseException):
+    "General Socket Exception"
+    def __init__(self, message, property, *args, **kwargs):
+        self.message=message
+        self.property=property
+        super().__init__(message,*args,**kwargs)
+    
+class client_protocol_mismatch(SocketException):
+    "The server protocol does not follow the steps that the client knows"
+    def __init__(self, message, property, *args, **kwargs):
+        super().__init__(message, property, *args, **kwargs)
+
+class decode_error(SocketException):
+    "The message type is incorrect from what was declared"
+    def __init__(self, message, property, *args, **kwargs):
+        super().__init__(message, property, *args, **kwargs)
+
+class ReconnectionFailure(SocketException):
+    "Failure to reconnect"
+    def __init__(self, message, property, *args, **kwargs):
+        super().__init__(message, property, *args, **kwargs)
+
+class KSocketException(BaseException):
+    "General KSocket Exception"
+    def __init__(self, message, property, *args, **kwargs):
+        self.message=message
+        self.property=property
+        super().__init__(message,*args,**kwargs)
